0baf9cbe9667923990bda9e6db5c9ecf457ba077
AbdiMK <63399762+abdimk@users.noreply.github.com>
2022-10-17 12:24:43 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+MUSIC_CHATS = [
+    -1234567891012,
+    -2345678910123
+]
+MUSIC_USERS = [1234567890]
+MUSIC_DELAY_DELETE_INFORM = 10
+MUSIC_INFORM_AVAILABILITY = (
+    "This bot only serves the specified group and"
+    "its members in private chat"
+)
+MUSIC_MAX_LENGTH = 10800
